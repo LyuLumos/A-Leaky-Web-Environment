0f2276194cf4c6e8dd4632ec4c93b2fa336831e7
LyuLumos <3208413453@qq.com>
2021-07-18 22:37:40 +0800

    提交 一个可以运行逻辑正常的环境（无漏洞）

--- README.md
+++ README.md
@@ -1,22 +1,21 @@
-# ![DIagonAlley-Logo](img/logo.png) DiagonAlley
+<h1>Basic Flask App - SQLite - Python</h1></br>
 
-## Rules
+python-flask-sqlite-shopping-sitesi</br>
+free little-template</br>
+python 3.7</br>
+</br>
+<b><u>#Project Run#</u></b></br>
+pip install flask</br>
+pip install flask_sqlalchemy</br>
+python app.py</br></br>
+database.db</br></br>
+![1](https://user-images.githubusercontent.com/25853989/50376648-0808e980-0621-11e9-956e-f8f8aef331f2.PNG)
+![screenshot_2018-12-22 anasayfa - little](https://user-images.githubusercontent.com/25853989/50376561-7351bc00-061f-11e9-8ad6-b473332d681c.png)
+![screenshot_2018-12-22 anasayfa - little 1](https://user-images.githubusercontent.com/25853989/50376560-7351bc00-061f-11e9-9339-3ee95a18fedc.png)
+![screenshot_2018-12-22 erkek balikci yaka kazak siyah - little](https://user-images.githubusercontent.com/25853989/50376562-7351bc00-061f-11e9-8146-02c0aadc74fe.png)
+![screenshot_2018-12-22 findback - little](https://user-images.githubusercontent.com/25853989/50376563-73ea5280-061f-11e9-910b-8eaa9aafcbf8.png)
+![screenshot_2018-12-22 sepetim - little 1](https://user-images.githubusercontent.com/25853989/50376564-73ea5280-061f-11e9-93ec-01a97dd230f5.png)
+![screenshot_2018-12-22 sepetim - little 2](https://user-images.githubusercontent.com/25853989/50376565-73ea5280-061f-11e9-8683-8ff3c41638b8.png)
+![screenshot_2018-12-22 sepetim - little](https://user-images.githubusercontent.com/25853989/50376566-73ea5280-061f-11e9-9291-b963ec88d112.png)
+![screenshot_2018-12-22 urun addnew - little](https://user-images.githubusercontent.com/25853989/50376567-7482e900-061f-11e9-9166-c153a203e515.png)
 
-- [创新实践能力团队赛评分标准](https://c4pr1c3.github.io/cuc-wiki/cp/assessment.html)
-
-## TBD
-- [讨论区](https://github.com/LyuLumos/A-Leaky-Web-Environment/discussions)
-
-## To-do list
-- [ ] 功能
-- [ ] 文档
-  - [ ] 需求分析、（概要和详细）设计文档
-  - [ ] API 接口设计说明
-  - [ ] 必要的插图或表格
-  - [ ] 人员分工说明
-- [ ] 其他
-  - [ ] EXP
-
-## Gantt Chart
-
-![WorkOuts](img/gantt-chart.png)
--- app.py
+++ app.py
@@ -1,89 +1,322 @@
-from flask import Flask, render_template, request, redirect, url_for
-import pymysql
-import traceback
-
+from flask import Flask, render_template, request, session, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import desc
+import hashlib
+import time
 app = Flask(__name__)
+app.secret_key = 'samet'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-# MySQL
-# create database if not exists Test_db;
-# use Test_db;
-# create table if not exists user 
-# (
-# user varchar(40),
-# password varchar(40)
-# );
 
-# insert into 
-#user (user, password) values('admin', 'fd063329ee736949b644e77b5919d1a2');
-# select * from user;
+class User(db.Model):
+    __tablename__ = "user"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.Text, primary_key=True, nullable=False)
+    passwd = db.Column(db.Integer)
+    email = db.Column(db.Text, unique=True, nullable=False)
+    auth = db.Column(db.Integer)
 
-# md5(english.encode('utf8')).hexdigest()
 
-# create user alice identified by '528113';
-# grant select, insert on Test_db.user to 'alice';
+class Category(db.Model):
+    __tablename__ = "category"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.Text, unique=True, nullable=False)
+
+
+class Product(db.Model):
+    __tablename__ = "product"
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    name = db.Column(db.Text, nullable=False)
+    perprice = db.Column(db.Integer, nullable=False)
+    store = db.Column(db.Integer, nullable=False)
+    sold = db.Column(db.Text)
+    piclink = db.Column(db.Text)
+    cateid = db.Column(db.Integer, db.ForeignKey('category.id'))
+
+
+class History(db.Model):
+    __tablename__ = "history"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'))
+    num = db.Column(db.Text, nullable=False)
+    perprice = db.Column(db.Text, nullable=False)
+    sumprice = db.Column(db.Text, nullable=False)
+    time = db.Column(db.Text, nullable=False)
+
+
+db.create_all()
+product = Product.query.all()
+category = Category.query.all()
+carts = []
+
 
 @app.route('/')
+def index():
+    product = Product.query.all()
+    category = Category.query.all()
+    # print(product, category)
+    return render_template('index.html', product=product, category=category)
+
+# 丢失界面
+@app.route('/lost')
+def lost():
+    return render_template('lost.html')
+
+# 登录
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    if request.method == 'GET':
+        if 'login_ok' in session:
+            if session['login_ok'] == True:
+                return redirect(url_for('index'))
+            else:
+                return render_template('login.html')
+        else:
+            session['login_ok'] = False
+            return redirect(url_for('login'))
+    else:
+        email = request.form['email']
+        passwd = request.form['pass']
+        sifrelenmis = hashlib.sha256(passwd.encode("utf8")).hexdigest()
+
+        print(sifrelenmis)
+        if User.query.filter_by(email=email, passwd=sifrelenmis).first():
+            veri = User.query.filter_by(
+                email=email, passwd=sifrelenmis).first()
+            session['login_ok'] = True
+            session['isim'] = veri.name
+            session['id'] = veri.id
+            session['auth'] = veri.auth
+            session['sepet'] = carts
+            return redirect(url_for('index'))
+        else:
+            return redirect(url_for('login'))
 
 
-@app.route('/regist')
-def regist():
-    return render_template('regist.html')
+@app.route('/logout')
+def logout():
+    session['login_ok'] = False
+    session.pop('cart', None)
+    session.pop('isim', None)
+    session.pop('id', None)
+    session.pop('auth', None)
+    carts.clear()
+    session['sepet'] = carts
+    return redirect(url_for('index'))
 
 
-@app.route('/registuser')
-def getRegistRequest():
-    db = pymysql.connect(host="localhost", user="root", passwd="123456", database="Test_db")
-    cursor = db.cursor()
-    sql = "INSERT INTO user(user, password) VALUES ('{user}', '{passwd}')" \
-        .format(user=str(request.args.get('user')),
-                passwd=str(request.args.get('password')))
-    try:
-        cursor.execute(sql)
-        db.commit()
+@app.route('/findback', methods=['GET', 'POST'])
+def findback():
+    if request.method == 'GET':
+        if 'login_ok' in session:
+            if session['login_ok'] == True:
+                return redirect(url_for('index'))
+            else:
+                return render_template('findback.html')
+        else:
+            session['login_ok'] = False
+            return render_template('findback.html')
+    else:
+        try:
+            isim = request.form['username']
+            email = request.form['email']
+            passwd = request.form['pass']
+            sifrelenmis = hashlib.sha256(passwd.encode("utf8")).hexdigest()
+            kullanıcı = User(name=isim, passwd=sifrelenmis,
+                             email=email, auth='0')
+            db.session.add(kullanıcı)
+            db.session.commit()
+            return redirect(url_for('index'))
+        except:
+            return redirect(url_for('findback'))
+
+
+
+@app.route('/cart')
+def cart():
+    if 'login_ok' in session:
+        if(session['login_ok'] == True):
+            sepetteki = session["sepet"]
+            category = Category.query.all()
+            return render_template('cart.html', sepetteki=sepetteki, category=category)
+        else:
+            return render_template('login.html')
+    else:
+        session['login_ok'] = False
         return redirect(url_for('login'))
-    except:
-        traceback.print_exc()
-        db.rollback()
-        return '注册失败'
-    db.close()
 
 
-# http://127.0.0.1:5000/login?user=1%27%20or%20%271%27=%271&password=1%27%20or%20%271%27=%271
-@app.route('/login')
-def getLoginRequest():
-    db = pymysql.connect(host="localhost", user="root", passwd="123456", database="Test_db")
-    cursor = db.cursor()
-    sql = "select * from user where user='{user}' and password= '{passwd}'" \
-        .format(user=str(request.args.get('user')),
-                passwd=str(request.args.get('password')))
-    print("sql=" + sql)
-    try:
-        cursor.execute(sql)
-        results = cursor.fetchall()
-        print("login success: " + str(len(results)))
-        if len(results) == 1:
-            return '登录成功'
+@app.route('/addcart/<urunid>')
+def addcart(urunid):
+    if 'login_ok' in session:
+        if(session['login_ok'] == True):
+            urungetir = Product.query.filter_by(id=urunid).first()
+            durum = False
+            carts = session["sepet"]
+            for bul in carts:
+                if str(bul['id']) == str(urunid):
+                    durum = True
+            if carts == []:
+                num = 1
+                hesap = num * 0.99
+                perprice = int(urungetir.perprice) + hesap
+                sumprice = num * perprice
+                addcart = {
+                    'id': urungetir.id,
+                    'name': urungetir.name,
+                    'perprice': perprice,
+                    'piclink': urungetir.piclink,
+                    'num': num,
+                    'sumprice': sumprice
+                }
+                carts.append(addcart)
+            elif durum == True:
+                cart = []
+                for bul in carts:
+                    if str(bul['id']) == str(urunid):
+                        num = int(bul["num"])
+                        num += 1
+                        hesap = num * 0.99
+                        perprice = int(bul['perprice'])
+                        sumprice = (perprice * num) + hesap
+                        bul['num'] = str(num)
+                        bul['sumprice'] = str(sumprice)
+                        cart.append(bul)
+                    else:
+                        cart.append(bul)
+            else:
+                num = 1
+                hesap = num * 0.99
+                perprice = int(urungetir.perprice) + hesap
+                sumprice = num * perprice
+
+                addcart = {
+                    'id': urungetir.id,
+                    'name': urungetir.name,
+                    'perprice': perprice,
+                    'piclink': urungetir.piclink,
+                    'num': num,
+                    'sumprice': sumprice
+                }
+                carts.append(addcart)
+            session["sepet"] = carts
+            return redirect(url_for('cart'))
         else:
-            return '用户名或密码不正确'
-        db.commit()
-    except:
-        traceback.print_exc()
-        db.rollback()
-    db.close()
+            return redirect(url_for('login'))
+    else:
+        session['login_ok'] = False
+        return redirect(url_for('login'))
 
 
-import time
-@app.route('/finalbuy',  methods=['GET', 'POST'])
-def buyFinalProduct():
-    if request.method == 'POST':
-        ans = str(request.args.get(['time']))
-        if int(ans) < int('3408192000'): # 2078.1.1 00:00:00
-            print("FAILED")
+@app.route('/delete/<urunid>')
+def delete(urunid):
+    if 'login_ok' in session:
+        if(session['login_ok'] == True):
+            cart = []
+            cart = session["sepet"]
+            carts.clear()
+            for sil in cart:
+                if str(sil['id']) != str(urunid):
+                    carts.append(sil)
+            session["sepet"] = carts
+            return render_template('cart.html', sepetteki=session["sepet"])
         else:
-            return render_template('flag.html')
+            return redirect(url_for('login'))
+    else:
+        session['login_ok'] = False
+        return redirect(url_for('login'))
 
+
+@app.route('/clearall', methods=['GET'])
+def clearall():
+    if 'login_ok' in session:
+        if(session['login_ok'] == True):
+            carts.clear()
+            session["sepet"] = carts
+            return redirect(url_for('cart'))
+        else:
+            return redirect(url_for('login'))
+    else:
+        session['login_ok'] = False
+        return redirect(url_for('login'))
+
+
+@app.route('/product/<urunid>')
+def urun(urunid):
+    if session['login_ok'] == True:
+        urundetay = Product.query.filter_by(id=urunid).first()
+        categories = Category.query.filter_by(id=urundetay.cateid).first()
+        category = Category.query.all()
+        return render_template('product.html', urundetay=urundetay, categories=categories, category=category)
+    else:
+        return redirect(url_for('login'))
+
+
+@app.route('/buy')
+def buy():
+    if 'login_ok' in session:
+        if(session['login_ok'] == True):
+            kid = session['id']
+            carts = session["sepet"]
+            for urun in carts:
+                product_id = int(urun["id"])
+                num = str(urun["num"])
+                urunn = Product.query.filter_by(id=product_id).first()
+                urunn.sold += int(num)
+                urunn.store -= int(num)
+                db.session.add(urunn)
+                db.session.commit()
+                timestamp = str(time.strftime("%x")+"-"+time.strftime("%X"))
+                perprice = urun["perprice"]
+                sumprice = urun["sumprice"]
+                histo = History(user_id=kid, product_id=product_id,
+                                  num=num, time=timestamp, perprice=perprice, sumprice=sumprice)
+                db.session.add(histo)
+                db.session.commit()
+            carts.clear()
+            session["sepet"] = carts
+            return redirect(url_for('cart'))
+        else:
+            return redirect(url_for('login'))
+    else:
+        session['login_ok'] = False
+        return redirect(url_for('login'))
+
+
+@app.route('/histo')
+def histo():
+    if 'login_ok' in session:
+        if(session['login_ok'] == True):
+            user_id = session["id"]
+            siparisverilen = History.query.filter_by(user_id=user_id)
+            veri = []
+            for itemi in siparisverilen:
+                uruncagir = Product.query.filter_by(id=itemi.product_id).first()
+                siparislerim = {
+                    'name': str(uruncagir.name),
+                    'perprice': itemi.perprice,
+                    'piclink': str(uruncagir.piclink),
+                    'num': itemi.num,
+                    'sumprice': itemi.sumprice,
+                    'time': itemi.time
+                }
+                veri.append(siparislerim)
+            return render_template('history.html', history=veri, category=category)
+        else:
+            return redirect(url_for('login'))
+    else:
+        session['login_ok'] = False
+        return redirect(url_for('login'))
+
+
+@app.route('/catege/<cateid>')
+def catege(cateid):
+    veri = Product.query.filter_by(cateid=cateid).order_by(desc(Product.sold))
+    category = Category.query.all()
+    return render_template('index.html', category=category, product=veri)
 
 
 if __name__ == '__main__':
